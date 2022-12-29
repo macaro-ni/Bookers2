422eb0e9a70da6cc2b5f34731bc1e69a3723da52
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2022-12-29 07:15:39 +0000

    [Add]ファイルの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
+  resources :users, only: [:index,:show,:edit]
+  resources :books, only: [:index,:show,:edit]
+  
   get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
