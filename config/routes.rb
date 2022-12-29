137c37f0cafc8c220a80e2cf7a08bd3668cbb2a3
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2022-12-29 10:00:03 +0000

    [Add]sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
+
+  root to: "homes#top"
+  devise_for :users
+
   resources :users, only: [:index,:show,:edit]
   resources :books, only: [:index,:show,:edit]
-  
-  get 'homes/top'
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
