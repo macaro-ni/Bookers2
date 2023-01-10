49b75ccbfd4ea8ef0fb44e242c246fd3257abf7a
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-01-10 04:04:08 +0000

    [add]user_info

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,20 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books =Book.all
   end
+
 
   def show
   end
 
   def edit
   end
+  
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)  
+  end
+  
 end
