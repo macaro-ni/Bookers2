52e6f82fd746a2c6f0cced5a430f7d662293c85a
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-01-11 07:56:04 +0000

    [add]機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,30 +3,37 @@ class BooksController < ApplicationController
     @book = Book.new
     @books =Book.all
   end
-  
+
   def create
     @book= Book.new(book_params)
     @book.user_id=current_user.id
-    @biik.save
-    redirect_to book_path
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
+  def destroy
+    @book=Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.new
   end
-  
-  
+
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title,:body)
-  
+  end
   #?
   def list_params
-    params.require(:list).permit(:title, :body, :image)  
+    params.require(:list).permit(:title, :body, :image)
   end
-  
+
 end
