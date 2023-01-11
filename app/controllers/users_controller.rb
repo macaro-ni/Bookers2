842a9de801b78fa899144d6b20809cdce692cba3
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-01-11 12:52:18 +0000

    [fix]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @user=current_user
   end
 
   def show
@@ -14,7 +15,13 @@ class UsersController < ApplicationController
 
   def update
     @user=User.find(params[:id])
-    @user.update
+    @user.update(user_params)
     redirect_to user_path(user.id)
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:introduction)
+  end
+
 end
