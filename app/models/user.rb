259f91e32c90d56f51cba68064908ad92e24efb7
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-01-16 08:04:52 +0000

    [test]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
  has_one_attached :profile_image
 
  validates :name, presence: true
- validates :introduction, presence: true
- validates :profile_image, presence: true
+
 
 
 
